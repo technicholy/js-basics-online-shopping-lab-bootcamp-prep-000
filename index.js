254f172cf18aa49d0dad26c1fb9aa079455ae19b
Clayton Pierce <bamer78@gmail.com>
2020-08-21 21:21:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function viewCart() {
   }else if(cart.length === 2){
     return `In your cart, you have ${cart[0].itemName} at \$${cart[0].itemPrice}, and ${cart[1].itemName} at \$${cart[1].itemPrice}.`
   }else{
-    lastItem = cart.pop()
+    var lastItem = cart.pop()
     retStr = ''
     while (cart.length > 0){
       retStr = retStr.concat(`${cart[0].itemName} at \$${cart[0].itemPrice}, `);
