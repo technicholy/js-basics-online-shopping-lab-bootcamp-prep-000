7b541b9b6046a7c3154dd80fd086798fcd684a13
Clayton Pierce <bamer78@gmail.com>
2020-08-21 21:22:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function viewCart() {
     var lastItem = cart.pop()
     var retStr = ''
     while (cart.length > 0){
-      retStr = retStr.concat(`${cart[0].itemName} at \$${cart[0].itemPrice}, `);
+      retStr = retStr.concat(`${cart[0][itemName]} at \$${cart[0][itemPrice]}, `);
       cart.shift()
     }
     return `In your cart, you have ${retStr} and ${lastItem}.`
