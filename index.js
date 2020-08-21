d0f0a76cd34fc182cba4d5056e3774f23c506c01
Clayton Pierce <bamer78@gmail.com>
2020-08-21 21:20:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,6 +24,16 @@ function viewCart() {
     return "Your shopping cart is empty."
   }else if(cart.length === 1){
     return `In your cart, you have ${cart[0].itemName} at \$${cart[0].itemPrice}.`
+  }else if(cart.length === 2){
+    return `In your cart, you have ${cart[0].itemName} at \$${cart[0].itemPrice}, and ${cart[1].itemName} at \$${cart[1].itemPrice}.`
+  }else{
+    lastItem = cart.pop()
+    retStr = ''
+    while (cart.length > 0){
+      retStr = retStr.concat(`${cart[0].itemName} at \$${cart[0].itemPrice}, `);
+      cart.shift()
+    }
+    return `In your cart, you have ${retStr} and ${lastItem}.`
   }
 }
 
