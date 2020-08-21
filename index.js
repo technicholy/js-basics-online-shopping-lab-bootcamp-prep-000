f3811e36fbe19b63e2399642269a256545a01d7c
Clayton Pierce <bamer78@gmail.com>
2020-08-21 20:58:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ function setCart(c) {
   cart = c;
   return cart;
 }
-
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 function addToCart(item) {
  var kart = {}
  kart.itemName = item
- kart.itemPrice = Math.floor(Math.Random(1, 100))
+ kart.itemPrice = getRandomInt(100)
+ 
 }
 
 function viewCart() {
