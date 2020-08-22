a125bfa715fef7d56fb2b6d30a6bcb83cd2fe098
Clayton Pierce <bamer78@gmail.com>
2020-08-22 01:47:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ function removeFromCart(item) {
   if (cart.includes(item)){
     for (let i =0; i<cart.length; i++){
       if (cart[i].itemName === item){
-        cart.splice(i, 1)
+        cart.splice(i)
       }
     }
   }else{
