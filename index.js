ee745c2aa31345ef0a8cbc1b2083aaaedb4f97f1
Clayton Pierce <bamer78@gmail.com>
2020-08-22 02:44:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function removeFromCart(item) {
 }
 function placeOrder(cardNumber) {
   if (cardNumber > 0){
-    console.log( `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`);
+    return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`;
     cart = []
   }else{
     return "Sorry, we don't have a credit card on file for you."
