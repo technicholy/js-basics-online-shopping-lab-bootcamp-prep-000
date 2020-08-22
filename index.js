4ba3e2a0458e716da2b6916e4298afb13179d9c6
Clayton Pierce <bamer78@gmail.com>
2020-08-22 02:38:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -43,7 +43,6 @@ function removeFromCart(item) {
   for (let i = 0; i< cart.length; i++){
     if (cart[i].itemName === item){
       cart.splice([i], 1)
-      return cart
     }
   }
   return "That item is not in your cart.";
