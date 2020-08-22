fd1e44c0ad5938b6621c49053f5d95378747532a
Clayton Pierce <bamer78@gmail.com>
2020-08-22 02:21:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -40,11 +40,11 @@ function total() {
 }
 
 function removeFromCart(item) {
-  const retCart = cart.filter(element => element.hasOwnProperty(item));
+  const retCart = cart.filter(element => !element.hasOwnProperty(item));
   if (retCart.length === cart.length){
     return "That item is not in your cart."
   }
-  var cart = retCart
+  cart = retCart
 }
 function placeOrder(cardNumber) {
   // write your code here
